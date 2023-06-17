afac0934ab2beecd5b4a6869eef4d2abf28431cb
Saad Zafar <70218806+saadzafar649@users.noreply.github.com>
2023-06-18 02:07:29 +0500

    react app start

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App= ()=>{
+  return(
+    <div>
+      <h1>Hello React Application</h1>
+      <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        ></a>
+    </div>
+  )
+}
+
+export default App
