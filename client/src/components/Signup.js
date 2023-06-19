86d85390298153347fb8df951a0ffef24ed1b355
Saad Zafar <70218806+saadzafar649@users.noreply.github.com>
2023-06-19 21:58:04 +0500

    feat: signup page ui

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,8 +1,68 @@
+import { Box, Card, CardActions, FormControl, FormGroup, Input, InputAdornment, InputLabel, TextField, Typography, Button } from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Mail from '@mui/icons-material/Mail';
+import Phone from '@mui/icons-material/Phone';
+import Password from '@mui/icons-material/Password';
+import PasswordTwoTone from '@mui/icons-material/PasswordTwoTone';
+// import '@mui/icons-material/index'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <Box sx={{ display: "flex", justifyContent: "center", overflow: 'clip', margin: 5 }}>
+
+      <Card sx={{ minWidth: 200, maxWidth: 800, width: 600, display: 'flex', justifyContent: "center", flexFlow: 'wrap' }}>
+          <Typography variant="h5" component="h5" sx={{ fontWeight: 700 }}>
+            Signup
+          </Typography>
+        <FormControl variant="standard" sx={{ fontWeight: 400, margin: 5, display: 'flex', justifySelf: "center", }}>
+
+          <Box sx={{ display: 'flex', alignItems: 'flex-end', margin: 2 }}>
+            <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField id="name" label="Name" variant="standard" sx={{ width: 400 }} />
+          </Box>
+
+
+          <Box sx={{ display: 'flex', alignItems: 'flex-end', margin: 2 }}>
+            <Mail sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField id="email" label="Email" variant="standard" type='email' sx={{ width: 400 }} />
+          </Box>
+
+          <Box sx={{ display: 'flex', alignItems: 'flex-end', margin: 2 }}>
+            <Phone sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField id="phone" label="Phone" variant="standard" type='phone' sx={{ width: 400 }} />
+          </Box>
+
+          <Box sx={{ display: 'flex', alignItems: 'flex-end', margin: 2 }}>
+            <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField id="profession" label="Profession" variant="standard" sx={{ width: 400 }} />
+          </Box>
+
+          <Box sx={{ display: 'flex', alignItems: 'flex-end', margin: 2 }}>
+            <PasswordTwoTone sx={{mr: 1, my: 0.5 }} />
+            <TextField id="password" label="Password" variant="standard" type='password' sx={{ width: 400 }} />
+          </Box>
+
+          <Box sx={{ display: 'flex', alignItems: 'flex-end', margin: 2 }}>
+            <Password sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField id="cpassword" label="ConfirmPassword" variant="standard" type='password' sx={{ width: 400 }} />
+          </Box>
+        </FormControl >
+        <CardActions>
+
+          <NavLink className="nav-link " aria-current="page" to="/login" sx={{ margin: 2, display: 'flex',justifySelf:'center' }}>
+            <Button key={1} color="primary" variant='contained'>
+              Signup
+            </Button>
+          </NavLink>
+        </CardActions>
+      </Card>
+    </Box>
   )
 }
 
